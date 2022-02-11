e5e1289741e22a58f2e2da009d1c1141592809bb
Yuuki <82380343+xcscxr@users.noreply.github.com>
2022-02-11 11:12:32 +0530

    Few exceptions handled

--- gdtot.py
+++ gdtot.py
@@ -1,4 +1,7 @@
-import requests, re, base64
+import re
+import base64
+import requests
+from urllib.parse import urlparse, parse_qs
 
 # file url
 URL = "" 
@@ -12,7 +15,8 @@ def parse_info(res):
     title = re.findall(">(.*?)<\/h5>", res.text)[0]
     info = re.findall('<td\salign="right">(.*?)<\/td>', res.text)
     parsed_info = {
-        'error': False,
+        'error': True,
+        'message': 'Link Invalid.',
         'title': title,
         'size': info[0],
         'date': info[1]
@@ -30,30 +34,45 @@ def gdtot_dl(url):
     info['src_url'] = url
 
     res = client.get(f"https://new.gdtot.top/dld?id={url.split('/')[-1]}")
-    matches = re.findall('gd=(.*?)&', res.text)
-
+    
     try:
-        decoded_id = base64.b64decode(str(matches[0])).decode('utf-8')
-        gdrive_url = f'https://drive.google.com/open?id={decoded_id}'
+        url = re.findall('URL=(.*?)"', res.text)[0]
     except:
-        info['error'] = True,
+        info['message'] = 'The requested URL could not be retrieved.',
+        return info
+
+    params = parse_qs(urlparse(url).query)
+    
+    if 'msgx' in params:
+        info['message'] = params['msgx'][0]
+    
+    if 'gd' not in params or not params['gd'] or params['gd'][0] == 'false':
+        return info
+    
+    try:
+        decoded_id = base64.b64decode(str(params['gd'][0])).decode('utf-8')
+        gdrive_url = f'https://drive.google.com/open?id={decoded_id}'
+        info['message'] = 'Success.'
+    except:
+        info['error'] = True
         return info
 
     info['gdrive_link'] = gdrive_url
     
     return info
-
+    
 # ==========================================
 
-output = gdtot_dl(URL)
+info = gdtot_dl(URL)
 
-print(output)
+print(info)
 
 # ==========================================
 '''
 SAMPLE OUTPUT:
 {
-    'error': False, 
+    'error': False,
+    'message': 'Some status message..' 
     'title': 'Filename on website', 
     'size': '627.03 MB', 
     'date': '11-Feb-2022 02:47:12', 
